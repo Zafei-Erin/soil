09b6ca0ea683b5cfcb001d6c17a5d70f22c586b8
Zafei-Erin <897659752@qq.com>
2024-05-21 23:23:33 +0800

    fix: token type and value

--- frontend/src/types/address.ts
+++ frontend/src/types/address.ts
@@ -2,11 +2,11 @@ const WETHAddress = import.meta.env.VITE_WETH;
 const DAIAddress = import.meta.env.VITE_DAI;
 const SOILAddress = import.meta.env.VITE_SOIL;
 
-export const Token = ["WETH", "DAI", "SOIL"] as const;
-export type Token = (typeof Token)[number];
+export const DepositToken = ["WETH", "DAI"] as const;
+export type DepositToken = (typeof DepositToken)[number];
 
-export const DepositToken = Token.filter((token) => token !== "SOIL");
-export type DepositToken = Exclude<Token, "SOIL">;
+export const Token = [...DepositToken, "SOIL"] as const;
+export type Token = (typeof Token)[number];
 
 export const tokenAddress: Record<Token, string> = {
   WETH: WETHAddress,
