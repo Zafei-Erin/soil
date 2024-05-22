b52f4db8d3af193c9a2452a2c062c3f9b64ee0df
Zafei-Erin <897659752@qq.com>
2024-05-22 23:16:36 +0800

    fix: healthfactor init value too big

--- frontend/src/hooks/useHealthFactor.ts
+++ frontend/src/hooks/useHealthFactor.ts
@@ -1,6 +1,7 @@
 import SOIL from "@/abis/SOIL.json";
 import { TokenAddress } from "@/constants/token";
 import { isValidChain } from "@/lib/utils";
+import { useBalances } from "@/provider/balanceProvider";
 import {
   useWeb3ModalAccount,
   useWeb3ModalProvider,
@@ -12,6 +13,7 @@ export function useHealthFactor() {
   const [healthFactor, setHealthFactor] = useState<number>(0);
   const { isConnected, address, chainId } = useWeb3ModalAccount();
   const { walletProvider } = useWeb3ModalProvider();
+  const { getBalances } = useBalances();
 
   const refreshHealthFactor = async () => {
     if (!isConnected || !walletProvider || !address) {
@@ -21,12 +23,19 @@ export function useHealthFactor() {
     if (!chainId || !isValidChain(chainId)) {
       throw Error("Chain not support");
     }
+
+    const soilBalance = getBalances("SOIL");
+    if (soilBalance == 0) {
+      setHealthFactor(0);
+      return;
+    }
+    
     const ethersProvider = new BrowserProvider(walletProvider);
     const signer = await ethersProvider.getSigner();
     const soilAddress = TokenAddress[chainId].SOIL;
     const contract = new Contract(soilAddress, SOIL.abi, signer);
     const result = await contract.getHealthFactor(address);
-    const healthFactor = parseFloat(formatUnits(result, 18));
+    const healthFactor = Math.min(parseFloat(formatUnits(result, 18)), 100);
     console.log("health factor", healthFactor);
     setHealthFactor(healthFactor);
   };
