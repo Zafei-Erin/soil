fec1944be75df2589d24e097c35300b96b7bf904
Zafei-Erin <897659752@qq.com>
2024-05-24 22:07:23 +0800

    style: header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,15 +1,29 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { ConnectButton } from "./ConnectButton";
+import { cn } from "@/lib/utils";
 
 export const Header = () => {
+  const { pathname } = useLocation();
   return (
-    <div className="h-20 flex items-center justify-between px-6 py-3">
-      <div className="flex gap-3 items-center">
-        <Link to="/">Mint and Borrow</Link>
-        <Link to="/dashboard">DashBoard</Link>
-        <Link to="/liquidate">Liquidate</Link>
+    <div className="h-20 flex items-center justify-center sm:justify-end sm:pr-32 py-3">
+      <div className="flex gap-3 items-center text-gray-400">
+        <Link to="/">
+          <span className={cn(pathname == "/" && "text-white")}>
+            Mint / Borrow
+          </span>
+        </Link>
+        <Link to="/dashboard">
+          <span className={cn(pathname == "/dashboard" && "text-white")}>
+            DashBoard
+          </span>
+        </Link>
+        <Link to="/liquidate">
+          <span className={cn(pathname == "/liquidate" && "text-white")}>
+            Liquidate
+          </span>
+        </Link>
+        <ConnectButton />
       </div>
-      <ConnectButton />
     </div>
   );
 };
