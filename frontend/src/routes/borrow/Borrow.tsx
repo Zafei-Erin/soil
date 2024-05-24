f25f9ebcb2a945f1ff9e3a3278a8ccc570c49cad
Zafei-Erin <897659752@qq.com>
2024-05-24 16:03:17 +0800

    refactor: mint token button

--- frontend/src/routes/borrow/Borrow.tsx
+++ frontend/src/routes/borrow/Borrow.tsx
@@ -1,9 +1,9 @@
-import MintWethButton from "@/components/MintWethButton";
-import { BorrowModal } from "./BorrowModal";
+import { MintTokenButton } from "@/components/MintTokenButton";
 import { UpdateSoilPriceModal } from "@/components/UpdateSoilPriceModal";
-import { useWeb3ModalAccount } from "@web3modal/ethers/react";
-import { isValidChain } from "@/lib/utils";
 import { ChainID } from "@/constants/chainId";
+import { isValidChain } from "@/lib/utils";
+import { useWeb3ModalAccount } from "@web3modal/ethers/react";
+import { BorrowModal } from "./BorrowModal";
 
 export const Borrow = () => {
   const { chainId } = useWeb3ModalAccount();
@@ -14,12 +14,15 @@ export const Borrow = () => {
         <div className="text-2xl font-semibold text-center text-gray-600">
           Slogan2
         </div>
-        <MintWethButton />
+        <div className="flex items-center justify-center gap-3">
+          <MintTokenButton token="WETH" />
+          <MintTokenButton token="DAI" />
+        </div>
       </div>
       <div className="flex flex-col gap-3 items-center justify-center">
         <BorrowModal />
         {chainId && isValidChain(chainId) && chainId !== ChainID.Optimism && (
-          <UpdateSoilPriceModal chainId={chainId}/>
+          <UpdateSoilPriceModal chainId={chainId} />
         )}
       </div>
     </div>
