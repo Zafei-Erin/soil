7ac8ff8ed43c722778aaa5d1bc2185c0349d0bd6
Zafei-Erin <897659752@qq.com>
2024-05-25 12:08:59 +0800

    feat: add hover effect for header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,19 +6,34 @@ export const Header = () => {
   const { pathname } = useLocation();
   return (
     <div className="h-20 flex items-center justify-center sm:justify-end sm:pr-32 py-3">
-      <div className="flex gap-3 items-center text-gray-400">
+      <div className="flex gap-3 items-center text-gray-400 font-satoshi">
         <Link to="/">
-          <span className={cn(pathname == "/" && "text-white")}>
+          <span
+            className={cn(
+              " hover:text-white",
+              pathname == "/" && "text-white font-medium"
+            )}
+          >
             Mint / Borrow
           </span>
         </Link>
         <Link to="/dashboard">
-          <span className={cn(pathname == "/dashboard" && "text-white")}>
+          <span
+            className={cn(
+              "hover:text-white",
+              pathname == "/dashboard" && "text-white font-medium"
+            )}
+          >
             DashBoard
           </span>
         </Link>
         <Link to="/liquidate">
-          <span className={cn(pathname == "/liquidate" && "text-white")}>
+          <span
+            className={cn(
+              "hover:text-white",
+              pathname == "/liquidate" && "text-white font-medium"
+            )}
+          >
             Liquidate
           </span>
         </Link>
