d9936012a37896c41b342f512b82698ce8d808f0
Zafei-Erin <897659752@qq.com>
2024-05-27 10:59:40 +0800

    style: health factor

--- frontend/src/routes/dashboard/HealthFactor.tsx
+++ frontend/src/routes/dashboard/HealthFactor.tsx
@@ -1,43 +1,70 @@
 import { cn } from "@/lib/utils";
 import { useHealthFactor } from "@/provider/healthFactorProvider";
+import { useEffect } from "react";
 
-type State = "INIT" | "BROKE" | "DANGER" | "SAFE";
+type State = "Init" | "BROKE" | "Risky" | "Moderate" | "Healthy";
 
 export const HealthFactor = () => {
   const { healthFactor } = useHealthFactor();
   const state: State =
     healthFactor == 0
-      ? "INIT"
+      ? "Init"
       : healthFactor < 1
       ? "BROKE"
+      : healthFactor < 2
+      ? "Risky"
       : healthFactor < 3
-      ? "DANGER"
-      : "SAFE";
+      ? "Moderate"
+      : "Healthy";
+  const textColor: string =
+    state == "Init"
+      ? "text-gray-400/80"
+      : state == "BROKE"
+      ? "text-red-600/80"
+      : state == "Risky"
+      ? "text-amber-600/80"
+      : state == "Moderate"
+      ? "text-amber-400/80"
+      : "text-emerald-600/80";
+
+  const bgColor: string =
+    state == "Init"
+      ? "bg-gray-400/80"
+      : state == "BROKE"
+      ? "bg-red-600/80"
+      : state == "Risky"
+      ? "bg-amber-600/80"
+      : state == "Moderate"
+      ? "bg-amber-400/80"
+      : "bg-emerald-600/80";
+
+  const width = Math.min(healthFactor, 5) * 20;
+
+  useEffect(() => {}, [healthFactor]);
   return (
     <div className="space-y-3">
       <div>
         <h1 className="text-2xl font-semibold">Health Factor</h1>
-        <p className="text-xs text-gray-600">
+        <p className="text-xs text-gray-400">
           If the health factor drops below 1, the liquidation of your collateral
           might be triggered.
         </p>
       </div>
-      <div
-        className={cn(
-          "text-4xl text-gray-600 font-semibold",
-          state == "INIT"
-            ? "text-gray-600/80"
-            : state == "BROKE"
-            ? "text-red-600/80"
-            : state == "DANGER"
-            ? " text-amber-600/80"
-            : " text-emerald-600/80"
-        )}
-      >
-        {healthFactor.toLocaleString(undefined, {
-          maximumFractionDigits: 2,
-          minimumFractionDigits: 2,
-        })}
+
+      <div className="flex items-end justify-between">
+        <div className={cn("text-4xl text-gray-400 font-semibold", textColor)}>
+          {healthFactor.toLocaleString(undefined, {
+            maximumFractionDigits: 2,
+            minimumFractionDigits: 2,
+          })}
+        </div>
+        <span className={cn("text-lg text-gray-400", textColor)}>{state}</span>
+      </div>
+      <div className={cn("h-3 w-full rounded-full bg-gray-600")}>
+        <div
+          className={cn("h-full rounded-full", bgColor)}
+          style={{ width: `${width}%` }}
+        />
       </div>
     </div>
   );
