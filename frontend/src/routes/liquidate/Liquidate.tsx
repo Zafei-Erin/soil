58e0afcc4ec43df97b4ab4cef5400cb533224b9f
Zafei-Erin <897659752@qq.com>
2024-05-27 21:23:45 +0800

    liquidate padding

--- frontend/src/routes/liquidate/Liquidate.tsx
+++ frontend/src/routes/liquidate/Liquidate.tsx
@@ -3,7 +3,7 @@ import { LiquidateCard } from "./LiquidateCard";
 export function Liquidate() {
   return (
     <div className="flex items-center justify-center h-full w-full">
-      <div className="max-w-xl">
+      <div className="max-w-xl px-6">
         <LiquidateCard />
       </div>
     </div>
